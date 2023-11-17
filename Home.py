7923386dda3a17257b4a3e070df2136c39005dad
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-17 23:23:13 +0700

    .

--- Home.py
+++ Home.py
@@ -13,12 +13,12 @@ def load_lottieurl(url: str):
 
 
 st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
-st.subheader("Natthinan Khemthong DATA SCIENCE NPRU")
+st.subheader("Wiriya Hemmala DATA SCIENCE NPRU")
 
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image('./pic/2.jpg')
+    st.image('./pic/1.jpg')
     lot3="https://lottie.host/347e3388-72e3-4b11-9fe8-49b2349c4a7a/GekjLnvyXb.json"
     lottie3 = load_lottieurl(lot3)
     st_lottie(lottie3)
