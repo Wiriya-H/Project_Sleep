ee571bb3b5de947f79131e1a487804f5c42e3c81
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 13:19:42 +0700

    Update 1 graph.py

--- pages/1 graph.py
+++ pages/1 graph.py
@@ -8,16 +8,14 @@ df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
 
 st.table(df.head())
 
-st.header("---")
+st.header("The relationship between (sex) and (Sleep Disorder)")
 
-grouped_data = df.groupby('Sleep Disorder')['Gender'].value_counts()
-reshaped_data = grouped_data.unstack()
-custom_colors = ['#ff20b6', '#6eedf2']
-fig, axes = plt.subplots(nrows=1, ncols=len(reshaped_data), figsize=(12, 4))
-for i, (sleep_disorder, counts) in enumerate(reshaped_data.iterrows()):
-    ax = axes[i]
-    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=custom_colors)
-    ax.set_title(f'Sleep Disorder {sleep_disorder}')
+grouped_data = df.groupby(['Sleep Disorder', 'Gender']).size().reset_index(name='count')
+pivot_table = pd.pivot_table(grouped_data, values='count', index='Sleep Disorder', columns='Gender', fill_value=0)
+fig, ax = plt.subplots(figsize=(15, 7))
+colors = ['#57b199', '#7fc15a', '#ffa53b']
+pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
+plt.axis('equal')
 st.pyplot(fig)
 
 st.header("---")
@@ -28,17 +26,3 @@ pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=['#57b199',
 plt.axis('equal') 
 st.pyplot(fig)
 
-st.header("The relationship between (sex) and (Sleep Disorder)")
-grouped_data = df.groupby(['Sleep Disorder', 'Gender']).size().reset_index(name='count')
-
-# Create a new DataFrame with Sleep Disorder and Gender as index, and count as values
-pivot_table = pd.pivot_table(grouped_data, values='count', index='Sleep Disorder', columns='Gender', fill_value=0)
-
-# Plot the pie chart
-fig, ax = plt.subplots(figsize=(15, 7))
-colors = ['#57b199', '#7fc15a', '#ffa53b']
-pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
-plt.axis('equal')
-
-# Display the plot in Streamlit
-st.pyplot(fig)
