6ac4122ce4fd008fe0f4223d3ab744fde03e3dc0
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 11:46:59 +0700

    Update 1 wordcloud.py

--- pages/1 wordcloud.py
+++ pages/1 wordcloud.py
@@ -9,19 +9,33 @@ df = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv')
 
 
 st.header("Positive ")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Assuming you have a DataFrame named df
+# Example:
+# df = pd.DataFrame({'Sleep Disorder': ['A', 'A', 'B', 'B', 'C', 'C'],
+#                    'Gender': ['Male', 'Female', 'Male', 'Female', 'Male', 'Female']})
+
+# Group by 'Sleep Disorder' and 'Gender' and get value counts
 grouped_data = df.groupby('Sleep Disorder')['Gender'].value_counts()
+
+# Reshape the data for plotting
 reshaped_data = grouped_data.unstack()
+
+# Plotting pie charts
 fig, axes = plt.subplots(nrows=1, ncols=len(reshaped_data), figsize=(15, 5))
+
 for i, (sleep_disorder, counts) in enumerate(reshaped_data.iterrows()):
     ax = axes[i]
-    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90)
+    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=['skyblue', 'lightcoral'])
     ax.set_title(f'Sleep Disorder {sleep_disorder}')
 
 plt.show()
 
 
 st.header("Negative ")
-df.pivot_table(index='BMI Category',columns='Sleep Disorder',aggfunc={'Sleep Disorder':'count'}).plot.pie()
+#df.pivot_table(index='BMI Category',columns='Sleep Disorder',aggfunc={'Sleep Disorder':'count'}).plot.pie()
 
 plt.axis('equal')
 plt.show()
