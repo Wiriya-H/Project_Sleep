99028deebae858f03a72512bc75e854c7653e271
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 12:50:49 +0700

    Update 1 wordcloud.py

--- pages/1 wordcloud.py
+++ pages/1 wordcloud.py
@@ -8,7 +8,7 @@ df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
 
 st.table(df.head())
 
-st.header("Positive ")
+st.header("---")
 
 grouped_data = df.groupby('Sleep Disorder')['Gender'].value_counts()
 reshaped_data = grouped_data.unstack()
@@ -20,10 +20,28 @@ for i, (sleep_disorder, counts) in enumerate(reshaped_data.iterrows()):
     ax.set_title(f'Sleep Disorder {sleep_disorder}')
 st.pyplot(fig)
 
-st.header("Negative ")
+st.header("---")
 
 pivot_table = df.pivot_table(index='BMI Category', columns='Sleep Disorder', aggfunc={'Sleep Disorder': 'count'})
 fig, ax = plt.subplots(figsize=(20, 10))
 pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7'])
 plt.axis('equal') 
 st.pyplot(fig)
+
+st.header("---")
+
+fig = px.histogram(sleep_data, x='Sleep Disorder',
+                   color='Sleep Disorder',
+                   facet_col='Stress Level',
+                   barmode='group',
+                   color_discrete_sequence=['white', '#4A235A', '#C39BD3'],
+                   opacity=0.8)
+
+fig.update_layout(title='<b>The effect of Stress Level on Sleep Disorder</b> ..', title_font={'size': 30},
+                  paper_bgcolor='#EBDEF0',
+                  plot_bgcolor='#EBDEF0')
+
+fig.update_yaxes(showgrid=False)
+
+# Display the Plotly figure in Streamlit
+st.plotly_chart(fig)
