47e1bb15048372693faad39388d1bbfe39ee2ceb
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 13:43:36 +0700

    Update 1 visualization.py

--- pages/1 visualization.py
+++ pages/1 visualization.py
@@ -18,6 +18,17 @@ pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
 plt.axis('equal')
 st.pyplot(fig)
 
+st.header("The relationship between (Age) and (Sleep Disorder)")
+
+grouped_data = df.groupby(['Sleep Disorder', 'Age']).size().reset_index(name='count')
+pivot_table = pd.pivot_table(grouped_data, values='count', index='Sleep Disorder', columns='Age', fill_value=0)
+fig, ax = plt.subplots(figsize=(15, 7))
+colors = ['#57b199', '#7fc15a', '#ffa53b']
+pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
+plt.axis('equal')
+st.markdown("# The relationship between (age) and (Sleep Disorder)", unsafe_allow_html=True)
+st.pyplot(fig)
+
 st.header("The relationship between (BMI) and (Sleep Disorder)")
 
 pivot_table = df.pivot_table(index='BMI Category', columns='Sleep Disorder', aggfunc={'Sleep Disorder': 'count'})
