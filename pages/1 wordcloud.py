0c9491ab21f465f2694e5a7ca37c71127d12d7ac
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 11:59:41 +0700

    Update 1 wordcloud.py

--- pages/1 wordcloud.py
+++ pages/1 wordcloud.py
@@ -8,16 +8,25 @@ df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
 
 
 st.header("Positive ")
+# Group by 'Sleep Disorder' and 'Gender' and get value counts
 grouped_data = df.groupby('Sleep Disorder')['Gender'].value_counts()
+
+# Reshape the data for plotting
 reshaped_data = grouped_data.unstack()
+
+# Specify the colors
+custom_colors = ['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7']
+
+# Create a Streamlit figure
 fig, axes = plt.subplots(nrows=1, ncols=len(reshaped_data), figsize=(15, 5))
+
 for i, (sleep_disorder, counts) in enumerate(reshaped_data.iterrows()):
     ax = axes[i]
-    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=['skyblue', 'lightcoral'])
+    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=custom_colors)
     ax.set_title(f'Sleep Disorder {sleep_disorder}')
-    
-st.pyplot(fig)
 
+# Display the figure using st.pyplot()
+st.pyplot(fig)
 
 st.header("Negative ")
 #df.pivot_table(index='BMI Category',columns='Sleep Disorder',aggfunc={'Sleep Disorder':'count'}).plot.pie()
