3c8ab69a89d59a79eb0bbfc3ffd53e4708edfd65
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 15:17:22 +0700

    Update 2 analysis.py

--- pages/2 analysis.py
+++ pages/2 analysis.py
@@ -6,10 +6,15 @@ from sklearn.tree import DecisionTreeClassifier
 df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
 st.table(df.head())
 
-dt1 = dt['Sleep Duration'].sum()
-dt2 = dt['Quality of Sleep'].sum()
-dt3 = dt['Physical Activity Level'].sum()
-dt4 = dt['Stress Level'].sum()
-dx = [dt1, dt2, dt3, dt4]
+df1 = df['Sleep Duration'].sum()
+df2 = df['Quality of Sleep'].sum()
+df3 = df['Physical Activity Level'].sum()
+df4 = df['Stress Level'].sum()
+dx = [df1, df2, df3, df4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
-st.bar_chart(dx2)
+ st.bar_chart(dx2)
+if st.button("show bar chart"):
+    st.bar_chart(dx2)
+    st.button("Not show bar chart")
+else :
+    st.button("Not show bar chart") 
