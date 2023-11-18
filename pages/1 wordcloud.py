a1b49ec9816a9d1552d667344fcced6fcf855cb9
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 12:14:28 +0700

    Update 1 wordcloud.py

--- pages/1 wordcloud.py
+++ pages/1 wordcloud.py
@@ -17,7 +17,7 @@ reshaped_data = grouped_data.unstack()
 custom_colors = ['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7']
 
 # Create a Streamlit figure
-fig, axes = plt.subplots(nrows=3, ncols=len(reshaped_data))
+fig, axes = plt.subplots(nrows=1, ncols=len(reshaped_data), figsize=(15, 5))
 
 for i, (sleep_disorder, counts) in enumerate(reshaped_data.iterrows()):
     ax = axes[i]
