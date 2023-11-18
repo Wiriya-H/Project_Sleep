fe26a453024c119df7a592418bf51c888c88e543
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 11:45:06 +0700

    .

--- pages/1 wordcloud.py
+++ pages/1 wordcloud.py
@@ -1,10 +1,7 @@
-pip install termcolor
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from termcolor import colored
+#from termcolor import colored
 
 
 # Add header row while reading a CSV file
@@ -15,17 +12,17 @@ st.header("Positive ")
 grouped_data = df.groupby('Sleep Disorder')['Gender'].value_counts()
 reshaped_data = grouped_data.unstack()
 fig, axes = plt.subplots(nrows=1, ncols=len(reshaped_data), figsize=(15, 5))
-custom_colors = ['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7']
+#custom_colors = ['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7']
 for i, (sleep_disorder, counts) in enumerate(reshaped_data.iterrows()):
     ax = axes[i]
-    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=custom_colors)
+    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90)
     ax.set_title(f'Sleep Disorder {sleep_disorder}')
 
 plt.show()
 
 
 st.header("Negative ")
-df.pivot_table(index='BMI Category',columns='Sleep Disorder',aggfunc={'Sleep Disorder':'count'}).plot.pie(autopct ='%1.1f%%',subplots=True,figsize=(20,10),colors=['#C39BD3','#D2B4DE','#EBDEF0','#F4ECF7'])
+df.pivot_table(index='BMI Category',columns='Sleep Disorder',aggfunc={'Sleep Disorder':'count'}).plot.pie(autopct ='%1.1f%%',subplots=True,figsize=(20,10))
 
 plt.axis('equal')
 plt.show()
