3648d658df316bd9e0dc671d44d165441935aab8
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 15:12:07 +0700

    Update 2 analysis.py

--- pages/2 analysis.py
+++ pages/2 analysis.py
@@ -11,7 +11,7 @@ dt2 = dt['Sleep Duration'].sum()
 dt3 = dt['Physical Activity Level'].sum()
 dt4 = dt['Stress Level'].sum()
 dx = [dt1, dt2, dt3, dt4]
-dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+dx2 = pd.DataFrame(dx, index=["dt1", "dt2", "dt3", "dt4"])
 
 if st.button("show bar chart"):
     st.bar_chart(dx2)
