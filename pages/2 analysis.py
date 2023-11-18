af6fecfced668e9d715c2082abbbcedfc689f608
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 17:26:11 +0700

    Update 2 analysis.py

--- pages/2 analysis.py
+++ pages/2 analysis.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+st.image('./pic/1.jpg')
+col1,col2 = st.columns(2)
+
+with col1:
+    st.header('วิริยะ เหมมาลา')
+with col2:
+    st.subheader('สาขาวิทยาการข้อมูล')
+    st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
+
 html_1 = """
 <div style="background-color:#0E2954;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้เบื้องต้น</h5></center>
@@ -9,15 +18,15 @@ st.markdown(html_1, unsafe_allow_html=True)
 st.markdown("")
 
 import pandas as pd
-df=pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
-st.write(df.head(10))
+dt=pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
+st.write(dt.head(10))
 
-df1 = df['Sleep Duration'].sum()
-df2 = df['Quality of Sleep'].sum()
-df3 = df['Physical Activity Level'].sum()
-df4 = df['Stress Level'].sum()
+dt1 = dt['Sleep Duration'].sum()
+dt2 = dt['Quality of Sleep'].sum()
+dt3 = dt['Physical Activity Level'].sum()
+dt4 = dt['Stress Level'].sum()
 
-dx = [df1, df2, df3, df4]
+dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
 
 if st.button("show bar chart"):
@@ -36,21 +45,20 @@ st.markdown("")
 
 
 ptlen = st.slider("กรุณาเลือกข้อมูล petal.length",0,10)
-ptwd = st.slider("กรุณาเลือกข้อมูล petal.widfh",0,10)
+ptwd = st.slider("กรุณาเลือกข้อมูล petal.width",0,10)
 
 splen = st.number_input("กรุณาเลือกข้อมูล sepal.length")
-spwd = st.number_input("กรุณาเลือกข้อมูล sepal.widfh")
+spwd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
 
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
-
 
 if st.button("ทำนายผล"):
    # ทำนาย
    #dt = pd.read_csv("./data/iris.csv") 
 
-   X = df.drop('Sleep Disorder', axis=1)
-   y = df["Sleep Disorder"]   
+   X = dt.drop('Sleep Disorder', axis=1)
+   y = dt.Sleep Disorder 
 
    Knn_model = KNeighborsClassifier(n_neighbors=3)
    Knn_model.fit(X, y)
@@ -61,14 +69,12 @@ if st.button("ทำนายผล"):
    st.write(Knn_model.predict(x_input))
    out=Knn_model.predict(x_input)
 
-    if out[0] == "Normal":
-        # st.image("./pic/iris.jpg")
-        st.header("Normal")
-    elif out[0] == "Sleep Apnea":
-        # st.image("./pic/iris2.jpg")
-        st.header("Sleep Apnea")
-    else:
-        st.header("Verginiga")
-    st.button("ไม่ทำนายผล")
-else:
+   if out[0]=="Normal":
+      st.header("Normal")
+   elif out[0]=="Sleep Apnea":
+      st.header("Sleep Apnea")
+   else:
+      st.header("Verginiga")
+   st.button("ไม่ทำนายผล")
+else :
     st.button("ไม่ทำนายผล")
