b7aca99c9737777911599969619553dbf88d0d98
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 16:24:21 +0700

    Update 1 visualization.py

--- pages/1 visualization.py
+++ pages/1 visualization.py
@@ -9,24 +9,24 @@ df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
 
 st.table(df.head())
 
-st.header("The relationship between (sex) and (SleepDisorder)")
+st.header("The relationship between (sex) and (Sleep Disorder)")
 
-grouped_data = df.groupby(['SleepDisorder', 'Gender']).size().reset_index(name='count')
-pivot_table = pd.pivot_table(grouped_data, values='count', index='SleepDisorder', columns='Gender', fill_value=0)
+grouped_data = df.groupby(['Sleep Disorder', 'Gender']).size().reset_index(name='count')
+pivot_table = pd.pivot_table(grouped_data, values='count', index='Sleep Disorder', columns='Gender', fill_value=0)
 fig, ax = plt.subplots(figsize=(15, 7))
 colors = ['#57b199', '#7fc15a', '#ffa53b']
 pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
 plt.axis('equal')
 st.pyplot(fig)
 
-st.header("The relationship between (Age) and (SleepDisorder)")
+st.header("The relationship between (Age) and (Sleep Disorder)")
 
-grouped_data = df.groupby(['SleepDisorder', 'Age']).size().reset_index(name='count')
+grouped_data = df.groupby(['Sleep Disorder', 'Age']).size().reset_index(name='count')
 st.bar_chart(grouped_data.set_index('Age'))
 
-st.header("The relationship between (BMI) and (SleepDisorder)")
+st.header("The relationship between (BMI) and (Sleep Disorder)")
 
-pivot_table = df.pivot_table(index='BMI Category', columns='SleepDisorder', aggfunc={'SleepDisorder': 'count'})
+pivot_table = df.pivot_table(index='BMI Category', columns='Sleep Disorder', aggfunc={'Sleep Disorder': 'count'})
 fig, ax = plt.subplots(figsize=(20, 10))
 pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=['#57b199', '#7fc15a', '#ffa53b', '#e33d25'])
 plt.axis('equal') 
