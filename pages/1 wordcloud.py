187d295d6463368be2c4ece884ffcf67b4043ccd
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 12:51:33 +0700

    .commit

--- pages/1 wordcloud.py
+++ pages/1 wordcloud.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
