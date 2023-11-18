e4bb2a41557e3c13a3c9acc13c364fa3ae80718e
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 13:46:24 +0700

    .

--- pages/1 visualization.py
+++ pages/1 visualization.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
@@ -21,13 +22,15 @@ st.pyplot(fig)
 st.header("The relationship between (Age) and (Sleep Disorder)")
 
 grouped_data = df.groupby(['Sleep Disorder', 'Age']).size().reset_index(name='count')
-pivot_table = pd.pivot_table(grouped_data, values='count', index='Sleep Disorder', columns='Age', fill_value=0)
-fig, ax = plt.subplots(figsize=(15, 7))
-colors = ['#57b199', '#7fc15a', '#ffa53b']
-pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
-plt.axis('equal')
-st.markdown("# The relationship between (age) and (Sleep Disorder)", unsafe_allow_html=True)
-st.pyplot(fig)
+
+fig = px.bar(grouped_data, x='Sleep Disorder', y='count', color='Age',
+             labels={'Sleep Disorder': 'Sleep Disorder', 'count': 'Count', 'Age': 'Age'},
+             color_discrete_sequence=px.colors.qualitative.Set2)
+
+fig.update_layout(title_text='The relationship between (age) and (Sleep Disorder)',
+                  title_font_size=20)
+
+st.plotly_chart(fig)
 
 st.header("The relationship between (BMI) and (Sleep Disorder)")
 
