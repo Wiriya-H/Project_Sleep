f4e91dc39c64c192b050fb1d2471a569b3306d05
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-18 12:37:20 +0700

    Update 1 wordcloud.py

--- pages/1 wordcloud.py
+++ pages/1 wordcloud.py
@@ -11,28 +11,22 @@ st.table(df.head())
 st.header("Positive ")
 
 grouped_data = df.groupby('Sleep Disorder')['Gender'].value_counts()
-
-# Reshape the data for plotting
 reshaped_data = grouped_data.unstack()
-
-# Specify the colors
 custom_colors = ['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7']
-
-# Create a Streamlit figure
 fig, axes = plt.subplots(nrows=1, ncols=len(reshaped_data), figsize=(12, 4))
-
 for i, (sleep_disorder, counts) in enumerate(reshaped_data.iterrows()):
     ax = axes[i]
     ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=custom_colors)
     ax.set_title(f'Sleep Disorder {sleep_disorder}')
-
 st.pyplot(fig)
 
 st.header("Negative ")
+
 pivot_table = df.pivot_table(index='BMI Category', columns='Sleep Disorder', aggfunc={'Sleep Disorder': 'count'})
 fig, ax = plt.subplots(figsize=(20, 10))
-pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7'])
-plt.axis('equal') 
+plot = pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=['#C39BD3', '#D2B4DE', '#EBDEF0', '#F4ECF7'])
+for pie in plot:
+    pie.set_wedgeprops({'linewidth': 0})
 
-# Display the figure using st.pyplot()
+plt.axis('equal') 
 st.pyplot(fig)
