812da45d09d126baf0665879a57d8afbdf79b3b9
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-19 15:02:07 +0700

    .

--- Home.py
+++ Home.py
@@ -21,16 +21,6 @@ html_1 = """
 st.markdown(html_1, unsafe_allow_html=True)
 st.markdown("")
 
-col1, col2, col3 = st.columns([1.5, 6, 1])
-
-with col1:
-    st.write("") 
-
-with col2:
-    st.image("./pic/44.png")
-
-with col3:
-    st.write("")
 
 
 
---
+++ pages/3 aboutme.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+col1, col2, col3 = st.columns([1.5, 6, 1])
+
+with col1:
+    st.write("") 
+
+with col2:
+    st.image("./pic/44.png")
+
+with col3:
+    st.write("")
