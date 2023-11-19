9608feaeb2366eaf4acce08583616b3192f6521f
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-19 12:53:57 +0700

    Update 1 visualization.py

--- pages/1 visualization.py
+++ pages/1 visualization.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
 
