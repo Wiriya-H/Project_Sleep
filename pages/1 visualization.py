54d490ef5ad3584731c4a7525eaa94ae023ec254
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-19 13:07:57 +0700

    Update 1 visualization.py

--- pages/1 visualization.py
+++ pages/1 visualization.py
@@ -34,4 +34,4 @@ st.pyplot(fig)
 st.header("-----")
 
 group_age = df.groupby(['Age', 'Sleep Disorder']).size().reset_index(name='count')
-st.line_chart(group_age.set_index('Sleep Disorder'), color='Sleep Disorder')
+st.line_chart(group_age.set_index('Sleep Disorder'))
