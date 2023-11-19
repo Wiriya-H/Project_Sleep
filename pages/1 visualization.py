dd4cd0a759712ec68f2b1aef109571d9a152b65e
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-19 12:58:10 +0700

    Update 1 visualization.py

--- pages/1 visualization.py
+++ pages/1 visualization.py
@@ -2,11 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
-pip install plotly-express
 
 df = pd.read_csv('./data/Sleep_health_and_lifestyle_dataset.csv')
 
