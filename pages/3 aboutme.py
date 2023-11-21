31f9f3c5d1c603a847edadb6bc734b8db0af8703
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-21 13:21:13 +0700

    Update 3 aboutme.py

--- pages/3 aboutme.py
+++ pages/3 aboutme.py
@@ -10,3 +10,12 @@ with col2:
 
 with col3:
     st.write("")
+    
+    
+html_1 = """
+<div style="background-color:#0E1117;border-top: 3px solid #ffffff;border-bottom: 3px solid #ffffff;">
+<center><h4>Wiriya Hemmala 64/44 007</h4></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")   
