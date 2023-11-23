05426d7719912c119689ad7dd1dc63411db7c636
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-11-23 14:48:36 +0700

    Update 3 aboutme.py

--- pages/3 aboutme.py
+++ pages/3 aboutme.py
@@ -6,7 +6,7 @@ st.image("./pic/44.png")
     
 html_1 = """
 <div style="background-color:#0E1117;border-top: 3px solid #ffffff;border-bottom: 3px solid #ffffff;">
-<center><h4>Wiriya Hemmala</h4><h4>644285007 64/44</h4><h4>Data science NPRU</h4></center>
+<center><h4>Wiriya Hemmala 644285007 64/44</h4><h4>Data science NPRU</h4></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
